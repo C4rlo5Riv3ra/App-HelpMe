cadc80f7539b719b516779458ce138429a6829d4
JUAN CARLOS <cjimenezjuancar@uss.edu.pe>
2024-11-01 12:02:17 -0500

    Carga del menu inicio

---
+++ app/src/main/java/com/example/helpme_app/InicioFragment.java
@@ -0,0 +1,77 @@
+package com.example.helpme_app;
+
+import android.os.Bundle;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.helpme_app.databinding.FragmentInicioBinding;
+
+public class InicioFragment extends Fragment {
+
+    private FragmentInicioBinding binding;
+
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+    private String mParam1;
+    private String mParam2;
+
+    public InicioFragment() {
+        // Constructor vacío requerido
+    }
+
+    public static InicioFragment newInstance(String param1, String param2) {
+        InicioFragment fragment = new InicioFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        binding = FragmentInicioBinding.inflate(inflater, container, false);
+
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+            int itemId = item.getItemId();
+
+            if (itemId == R.id.inicioFragment) {
+                replaceFragment(new InicioFragment());
+                return true;
+            } else if (itemId == R.id.carrito) {
+                replaceFragment(new ComprasFragment());
+                return true;
+            } else if (itemId == R.id.chat) {
+                replaceFragment(new ChatFragment());
+                return true;
+            } else if (itemId == R.id.perfil) {
+                replaceFragment(new PerfilFragment());
+                return true;
+            }
+            return false;
+        });
+
+        return binding.getRoot();
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getChildFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout, fragment);
+        fragmentTransaction.commit();
+    }
+}
