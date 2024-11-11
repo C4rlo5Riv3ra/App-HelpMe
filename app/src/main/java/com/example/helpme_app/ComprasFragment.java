d392d30c456c277de54c7018e119640a4711f90b
bryan <briansoberonq@gmail.com>
2024-11-11 02:33:35 -0500

    Se realizo interfaz de perfilfragment. Se modificó la seccion del botonaigation (carrito) por asesorias, buscar asesorias.

--- app/src/main/java/com/example/helpme_app/ComprasFragment.java
+++ app/src/main/java/com/example/helpme_app/ComprasFragment.java
@@ -59,6 +59,6 @@ public class ComprasFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_compras, container, false);
+        return inflater.inflate(R.layout.fragment_asesoria, container, false);
     }
 }
